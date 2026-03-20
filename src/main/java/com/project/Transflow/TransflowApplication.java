239dd85c7eb91e1b0a171d6794395bba953ef22b
seowon <seowon1112@gmail.com>
2026-03-20 20:07:54 +0900

    feat: 백엔드 핫 픽스 진행

--- src/main/java/com/project/Transflow/TransflowApplication.java
+++ src/main/java/com/project/Transflow/TransflowApplication.java
@@ -3,10 +3,13 @@ package com.project.Transflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TransflowApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(TransflowApplication.class, args);
     }
 
